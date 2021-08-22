5ef3924fdfdb26a65df1ae67880ac255213f084b
B-gowri-sankar <sankaryadavrgukt@gamil.com>
2021-08-22 16:17:54 +0530

    get stories of public of single user

--- routes/stories.js
+++ routes/stories.js
@@ -126,4 +126,24 @@ router.get("/:id", ensureAuth, async (req, res) => {
 	}
 });
 
+//@desc User Profile
+//@route GET /user/:id
+
+router.get("/user/:id", async (req, res) => {
+	try {
+		const stories = await Story.find({
+			user: req.params.id,
+			status: "public",
+		})
+			.populate("user")
+			.lean();
+		res.render("stories/index", {
+			stories,
+		});
+	} catch (err) {
+		console.error(err);
+		return res.render("error/500");
+	}
+});
+
 module.exports = router;
