e5bcc5db52003467d64bb3239f9e6269d9b144cf
B-gowri-sankar <sankaryadavrgukt@gamil.com>
2021-08-22 15:56:28 +0530

    update fuctionality and delete icon added

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const path = require("path");
 const passport = require("passport");
 const session = require("express-session");
 const MongoDBStore = require("connect-mongodb-session")(session);
+const methodOverride = require("method-override");
 //load config file
 dotenv.config({ path: "./config/config.env" });
 
@@ -22,6 +23,18 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//Method override
+
+app.use(
+	methodOverride(function (req, res) {
+		if (req.body && typeof req.body === "object" && "_method" in req.body) {
+			let method = req.body._method;
+			delete req.body._method;
+			return method;
+		}
+	})
+);
 
 //Logging
 if (process.env.NODE_ENV === "development") {
--- routes/stories.js
+++ routes/stories.js
@@ -69,4 +69,20 @@ router.get("/edit/:id", ensureAuth, async (req, res) => {
 	}
 });
 
+//@desc update story
+//@route PUT /stories/:id
+
+router.put("/add/:id", ensureAuth, async (req, res) => {
+	let story = await Story.findById(req.params.id).lean();
+	if (!story) return res.render("error/404");
+	if (story.user != req.user.id) res.redirect("/stories");
+	else {
+		story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+			new: true,
+			runValidators: true,
+		});
+		res.redirect("/dashboard");
+	}
+});
+
 module.exports = router;
